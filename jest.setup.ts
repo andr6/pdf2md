ed3c612845dd620d3220943959eb6efa617e5202
Xpwd <333048+andr6@users.noreply.github.com>
2025-06-22 19:53:31 +0100

    fix: type compatibility for File.arrayBuffer

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,8 +1,8 @@
 import '@testing-library/jest-dom'
 
 if (!(File.prototype as any).arrayBuffer) {
-  File.prototype.arrayBuffer = async function () {
+  File.prototype.arrayBuffer = async function (): Promise<ArrayBuffer> {
     const text = await this.text()
-    return new TextEncoder().encode(text).buffer
+    return new TextEncoder().encode(text).buffer as ArrayBuffer
   }
 }
